6ae2cbcc6e2ea547a8aaaef95152d4a3dc7ec949
TopScrech <sergei_saliukov@icloud.com>
2025-07-13 00:19:50 +0200

    Initial Commit

---
+++ Archive Pro/Archive_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Archive_ProApp.swift
+//  Archive Pro
+//
+//  Created by Sergei Saliukov on 13.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct Archive_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Archive Pro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Archive Pro
+//
+//  Created by Sergei Saliukov on 13.07.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
